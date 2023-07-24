cdf025dd2cbf15a4e33855bbfd5898fd708cac52
gautamu633 <gautamu633@gmail.com>
2023-07-24 10:05:33 +0530

    who

--- gautam.js
+++ gautam.js
@@ -1,8 +1,8 @@
- // Data for the chart
+
  const xValues = Array.from({ length: 7 }, (_, index) => 20 + index * 5); // X-axis values from 20 to 50 with a difference of 5
  const yValues = Array.from({ length: 7 }, (_, index) => (index + 1) * 50); // Y-axis values from 50 to 350 with an increment of 50
 
- // Create the chart
+ 
  const ctx = document.getElementById("stackedBarChart").getContext("2d");
  new Chart(ctx, {
    type: "bar",
